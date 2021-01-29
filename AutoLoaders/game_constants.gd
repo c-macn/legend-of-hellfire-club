03d82a4ef8f78846fdaba358c9041f5ad8fb7232
c-macn <christopher.mcnulty891@gmail.com>
2021-01-29 16:55:33 +0000

    Added dictionary to link spawn locations for each scene

--- AutoLoaders/game_constants.gd
+++ AutoLoaders/game_constants.gd
@@ -6,10 +6,22 @@ enum SCENES {
 	HALLWAY_ROOM = 2,
 	DEMON_ROOM = 3,
 	SERVANTS_ROOM = 4,
-	GAME_ROOM = 5
+	GAME_ROOM = 5,
+	FINAL_ROOM = 6
 }
 
-var _scenes = ["main_room", "storage_room", "main_hallway_room", "demon_eye_room", "servants_room", "game_room"]
+var _scenes = ["main_room", "storage_room", "main_hallway_room", "demon_eye_room", "servants_room", "game_room", "final_room"]
+
+var _scene_spawn_points = {
+	"demon_eye_room": {},
+	"main_hallway_room": {},
+	"servants_room": {},
+	"game_room": {},
+	"storage_room": {}
+}
 
 func get_scene_name(scene_index: int) -> String:
 	return _scenes[scene_index]
+
+func get_scene_spawn_point(scene_name: String, direction: String) -> String:
+	return _scene_spawn_points.get(scene_name).get(direction)
