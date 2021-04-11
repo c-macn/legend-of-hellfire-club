7dcbca7af84c45d37135723e1e01933a68e87226
c-macn <christopher.mcnulty891@gmail.com>
2021-04-11 22:41:20 +0100

    TG-13 cultitst will stop to cast spell and resume chase after cooldown

--- Characters/Cultist/Cultist.gd
+++ Characters/Cultist/Cultist.gd
@@ -14,13 +14,15 @@ enum FACING_VALUES {
 	DIAGONAL_DOWN_RIGHT = 45
 }
 
-export(int) var detection_length = 100
-export(int) var detection_rays_length = 32
+# export(int) var detection_length = 100
+# export(int) var detection_rays_length = 32
+
+var BlessedShot: PackedScene = preload("res://Entities/BlessedWaterShot/BlessedShot.tscn")
 
 var velocity: Vector2 = Vector2()
 var speed: int = SPEED_PATROLLING
 var is_stunned: bool = false
-var is_chasing: bool = false
+var is_chasing: bool = true
 var player_position: Vector2 = Vector2.ZERO
 
 # steering behaviour
@@ -31,29 +33,31 @@ var chosen_direction := Vector2.ZERO
 var steer_force := 100.0
 
 onready var stun_timer: Timer = $StunnedTimer
-onready var detection_timer: Timer = $DetectionArea/DetectionTimer
-onready var detection_radius: Area2D = $DetectionArea
+onready var detection_timer: Timer = $DetectionTimer
+onready var cast_timeout: Timer = $CastTimeout
 onready var banishment_area: Area2D = $BanishmentArea
 onready var animation_player: AnimationPlayer = $AnimationPlayer
 onready var animated_sprite: AnimatedSprite = $AnimatedSprite
 
 func _ready():
 	add_to_group("cultists")
-	init_ai_context()
+	#init_ai_context()
 
 	stun_timer.connect("timeout", self, "_on_Stun_timeout")
 	detection_timer.connect("timeout", self, "_on_Detection_timeout")
-	detection_radius.connect("body_entered", self, "_on_Body_entered")
-	detection_radius.connect("body_exited", self, "_on_Body_exited")
+	cast_timeout.connect("timeout", self, "_on_Cast_cooldown")
 	banishment_area.connect("body_entered", self, "_on_Banish_entered")
 
 func _physics_process(delta):
 	if is_stunned:
 		pass
-	
-	velocity += seek_Saoirse() * delta
-	velocity = velocity.clamped(speed)
-	position += velocity * delta
+
+	if is_chasing:
+		velocity += seek_Saoirse() * delta
+		velocity = velocity.clamped(speed)
+		position += velocity * delta
+	else:
+		velocity = lerp(velocity, Vector2.ZERO, 0.5)
 	# set_interest()
 	# set_danger()
 	# choose_direction()
@@ -66,10 +70,10 @@ func _physics_process(delta):
 func stun():
 	if not is_stunned:
 		stun_timer.start()
-		speed = 0
-		if is_chasing:
-			is_chasing = false
-			player_position = Vector2.ZERO
+		# speed = 0
+		# if is_chasing:
+		# 	is_chasing = false
+		# 	player_position = Vector2.ZERO
 		animation_player.play("stunned")
 
 func get_player_position() -> Vector2:
@@ -101,75 +105,26 @@ func get_animation() -> String:
 		_:
 			return "default"
 
-func is_body_Saoirse(name: String) -> bool:
-	return name == "Saoirse"
-
 func _on_Stun_timeout() -> void:
 	animation_player.stop()
 	animated_sprite.visible = true
 	speed = SPEED_PATROLLING
 	is_stunned = false
 
-# detection radius entered
-func _on_Body_entered(_body: KinematicBody2D) -> void:
-	pass
-	# if not is_chasing:
-		# if is_body_Saoirse(body.name):
-		# 	detection_timer.start()
-
-# detection radius exited
-func _on_Body_exited(_body: KinematicBody2D) -> void:
-	pass
-	# if is_body_Saoirse(body.name):
-	# 	if not is_chasing:
-	# 		detection_timer.stop()
-	# 	else:
-	# 		is_chasing = false
-
-func _on_Banish_entered(_body: KinematicBody2D) -> void:
-	pass
-	# if is_body_Saoirse(body.name):
-	# 	body.banish()
+func _on_Banish_entered(body: KinematicBody2D) -> void:
+	if body:
+		detection_timer.start()
 
 func _on_Detection_timeout() -> void:
-	is_chasing = true
+	# fire magic shiz
+	cast_spell()
 	detection_timer.stop()
 
-# sets up the detection raycast, interest, and danger value arrays
-func init_ai_context() -> void:
-	interest.resize(detection_rays_length)
-	danger.resize(detection_rays_length)
-	detection_ray_directions.resize(detection_rays_length)
-
-	# create rays starting from rotation 0 (right facing)
-	for i in detection_rays_length:
-		var angle = i * 2 * PI / detection_rays_length
-		detection_ray_directions[i] = Vector2.RIGHT.rotated(angle)
-
-func set_interest() -> void:
-	var Saoirse_position = get_player_position()
-	for i in detection_rays_length:
-		var direction = detection_ray_directions[i].dot(Saoirse_position)
-		interest[i] = direction
-
-func set_danger() -> void:
-	var space_state = get_world_2d().direct_space_state
-	for i in detection_rays_length:
-		var ray_length = detection_ray_directions[i] * detection_length 
-		var result = space_state.intersect_ray(position, position + ray_length, [self])
-
-		danger[i] = 1.0 if result else 0.0
-
-func choose_direction() -> void:
-	for i in detection_rays_length:
-		if danger[i] > 0.0:
-			interest[i] = 0.0;
-
-	chosen_direction = Vector2.ZERO
-	for i in detection_rays_length:
-		chosen_direction += detection_ray_directions[i] * interest[i]
-
-	chosen_direction = chosen_direction.normalized()
+func cast_spell() -> void:
+	#var shot = BlessedShot.instance()
+	print("CAST")
+	is_chasing = false
+	cast_timeout.start()
 
 func seek_Saoirse() -> Vector2:
 	var steer = Vector2.ZERO
@@ -180,3 +135,45 @@ func seek_Saoirse() -> Vector2:
 		steer = (desired_direction - velocity).normalized() * steer_force
 	
 	return steer
+	
+func _on_Cast_cooldown() -> void:
+	is_chasing = true
+	cast_timeout.stop()
+	
+############### CONTEXT STEERING CODE #############################
+
+# sets up the detection raycast, interest, and danger value arrays
+# func init_ai_context() -> void:
+# 	interest.resize(detection_rays_length)
+# 	danger.resize(detection_rays_length)
+# 	detection_ray_directions.resize(detection_rays_length)
+
+# 	# create rays starting from rotation 0 (right facing)
+# 	for i in detection_rays_length:
+# 		var angle = i * 2 * PI / detection_rays_length
+# 		detection_ray_directions[i] = Vector2.RIGHT.rotated(angle)
+
+# func set_interest() -> void:
+# 	var Saoirse_position = get_player_position()
+# 	for i in detection_rays_length:
+# 		var direction = detection_ray_directions[i].dot(Saoirse_position)
+# 		interest[i] = direction
+
+# func set_danger() -> void:
+# 	var space_state = get_world_2d().direct_space_state
+# 	for i in detection_rays_length:
+# 		var ray_length = detection_ray_directions[i] * detection_length 
+# 		var result = space_state.intersect_ray(position, position + ray_length, [self])
+
+# 		danger[i] = 1.0 if result else 0.0
+
+# func choose_direction() -> void:
+# 	for i in detection_rays_length:
+# 		if danger[i] > 0.0:
+# 			interest[i] = 0.0;
+
+# 	chosen_direction = Vector2.ZERO
+# 	for i in detection_rays_length:
+# 		chosen_direction += detection_ray_directions[i] * interest[i]
+
+# 	chosen_direction = chosen_direction.normalized()
