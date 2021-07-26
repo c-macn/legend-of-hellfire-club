d84b4cca9a91652747ff77c9fcb1bf80b6edd878
c-macn <christopher.mcnulty891@gmail.com>
2021-07-26 17:59:31 +0100

    Filled out credits section, added surge hosting for sfx attribution

---
+++ Interface/Credits/Credits.gd
@@ -0,0 +1,12 @@
+extends Control
+
+onready var main_credits = $NinePatchRect/MainCredits
+onready var additional_code = $NinePatchRect/AdditionaCodeCredits
+onready var additional_resouces = $NinePatchRect/AdditionalResouceCredits
+
+func _ready() -> void:
+	pass
+
+
+func _on_NextButton_pressed():
+	pass
